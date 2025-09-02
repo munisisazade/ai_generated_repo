a2a8cabc1d73bbcaa1d153a6d6c7d4411cdd9764
munisisazade <munisisazade@gmail.com>
2025-09-02 23:32:00 +0000

    forges quokka 🧠

--- stream_20250902_233053_nues6o.py
+++ stream_20250902_233053_nues6o.py
@@ -1,1 +1,2 @@
 fibonacci = lambda n: 1 if n <= 1 else fibonacci(n-1) + fibonacci(n-2)  # Recursive function to calculate Fibonacci numbers
+fizz_buzz = [(i, "Fizz"*(i%3==0) + "Buzz"*(i%5==0) or i) for i in range(1, 101)]  # classic interview problem
